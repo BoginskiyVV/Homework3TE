9688356c253e1a783ae8a6b8912083d21fd63777
BoginskiyVV <vov4ik84@gmail.com>
2022-10-24 21:02:11 +0300

    Task 04

---
+++ Task04.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
+# 45 -> 101101
+# 3 -> 11
+# 2 -> 10
+
+def con_bynary(x):
+    while x > 0:
+        last = x % 2
+        # print(last)
+        q.append(last)
+        x = x // 2
+
+x = int(input('Для преобразования введите число: '))
+q = []
+
+con_bynary(x)
+t = q[::-1]
+
+res = int(str(t).replace(' ', '').replace('[', '').replace(']', '').replace(',', ''))
+print(f'{x} -> {res}')
